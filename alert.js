b81d45dbb451d237d99cc9e00e1ae9b77889de7d
Rafayel Khachatryan <51332911+RafayelKhachatryan@users.noreply.github.com>
2022-11-20 16:23:49 +0400

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,43 @@
+/*let a, b;
+a = prompt("input number a = ");
+b = prompt("input number b = ");
+
+alert(+a + +b);
+alert(+a - +b);
+alert(+a * +b);
+alert(+a / +b);
+alert(+a % +b);*/
+
+/*
+let namee, age;
+namee = prompt("What is your name?");
+age = prompt("how old are you?");
+alert(`My name is ${namee}, I am ${age} years old`);
+*/
+
+/*let num = prompt("Enter a number");
+if(num <0 ){
+alert("YES");
+}
+ else{
+    alert("NO");
+ }*/
+
+ /*let num = prompt("Enter a number");
+ if(num % 2 == 0 ){
+ alert("even");
+ }
+  else{
+     alert("odd");
+  }*/
+
+ /* let num1, num2;
+  num1 = prompt("Enter a number");
+  num2 = prompt("Enter a number");
+
+ if(num1 % num2 === 0 || num2 % num1 === 0){
+ alert("1");
+ }
+  else{
+     alert("0");
+  }*/
